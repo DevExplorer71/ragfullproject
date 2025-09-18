c37950ed6aaede5de84c01794a2cb7080cb1b5d2
SciGama <techinnovator71@gmail.com>
2025-09-18 13:21:05 -0400

    Clear Firebase configuration object
    Removed sensitive Firebase configuration details.

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD1NrJ93oypp3ZZJA1Rna3CwDDFcnMziHs",
-  authDomain: "myragexample.firebaseapp.com",
-  projectId: "myragexample",
-  storageBucket: "myragexample.firebasestorage.app",
-  messagingSenderId: "189231428527",
-  appId: "1:189231428527:web:0595fdd067a1b278f0b55a",
-  measurementId: "G-J5FXL4N367"
-};
+const firebaseConfig = {}
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
